1dd73b3b637fa80806b434658cd03a15cd6f24b9
mso <markus.sonderegger@massiveart.com>
2013-08-29 11:37:23 +0200

    add LICENSE

--- polling.go
+++ polling.go
@@ -1,3 +1,8 @@
+// (c) MASSIVE ART WebServices GmbH
+//
+// This source file is subject to the MIT license that is bundled
+// with this source code in the file LICENSE.
+
 package engineio
 
 import (
